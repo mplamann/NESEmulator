195c5c87b4b719cc4b6abfbcb84bc6b313791ccf
Mitchell Plamann <knightquestrpg@gmail.com>
2012-04-15 13:44:55 -0500

    HOME AT LAST

--- src/MemoryState.cpp
+++ src/MemoryState.cpp
@@ -7,14 +7,6 @@ MemoryState::MemoryState(void)
 {
   for (int i = 0; i < RAM_SIZE; i++)
     RAM[i] = 0;
-  RAM[0] = 0xA9; // LDA #23
-  RAM[1] = 0x17;
-  RAM[2] = 0x60; // ADC $0A00
-  RAM[3] = 0x00;
-  RAM[4] = 0x0A;
-  RAM[5] = 0x29;
-  RAM[6] = 0x08;
-  RAM[0x0A00] = 0x10;
 }
 
 MemoryState::~MemoryState(void)
